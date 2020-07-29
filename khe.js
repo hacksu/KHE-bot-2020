948ebf04549fae8ff2f35c4e90cd9f4dacefdf30
bmvcbr <kathmanduklc@gmail.com>
2020-07-29 15:49:22 -0400

    added /guild command to make a category with a text and voice channel for sponsors

--- khe.js
+++ khe.js
@@ -11,11 +11,43 @@ client.on('ready', () => {
     msg.reply("you asked me to do stuff, here's a math problem: 2 + 2 = 4")
   }
 */
+console.log("above client.on");
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
+client.on('message', message => {
+    command = message.content.split(" ")[0];
+    console.log(command);
+    if (command === '/ping') {
+        message.reply('pong');
+    }
+
+    if (command === '/guild') {
+      (async () => {
+        let name = message.content.split(' ')[1];
+        let category = await message.guild.channels.create(name, {
+          type: 'category',
+          permissionOverwrites: [{
+            id: message.guild.id,
+            deny: ['MANAGE_MESSAGES'],
+            allow: ['SEND_MESSAGES'],
+          }],
+        });
+        let textChannel = await message.guild.channels.create(name, { type: 'text', });
+        let voiceChannel = await message.guild.channels.create(name, { type: 'voice', });
+        await textChannel.setParent(category.id);
+        await voiceChannel.setParent(category.id);
+        message.reply('Channels Created');
+      })();
+    }
+
+    if (command === '/help') {
+      message.reply(`KHE Commands: 
+                 /ping - bot responds with pong 
+                 /guild - creates a guild with a text and voice channel based on a given name`);
+    }
+
   }
-});
+
+
+);
 
 client.login(token);
